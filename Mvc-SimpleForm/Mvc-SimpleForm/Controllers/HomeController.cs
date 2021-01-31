04ebaf9bc3ddefea0b0cd439e55172b1717511e8
Bahtiyar <bahtiyarsime@gmail.com>
2021-01-31 22:22:35 +0300

    Basic Form
    Simple Form Example

---
+++ Mvc-SimpleForm/Mvc-SimpleForm/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_SimpleForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// GET:Home
+        /// Returns Home Page view
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Post: Home
+        /// </summary>
+        /// <param name="text1"></param>
+        /// <param name="check1"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Index(string text1,bool check1)
+        {
+            /*also you can get values like this 
+             
+             string text1=Request.Form["text1"];
+             bool check1=Request.Form.GetValues("check1")[0]
+             
+             */
+            return View();
+        }
+    }
+}
---
+++ Mvc-SimpleForm/Mvc-SimpleForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mvc_SimpleForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Turkcell Iletisim Hizmetleri A.S.")]
+[assembly: AssemblyProduct("Mvc_SimpleForm")]
+[assembly: AssemblyCopyright("Copyright © Turkcell Iletisim Hizmetleri A.S. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("127e5ae7-b980-430b-bc51-dedbb82aa40a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
